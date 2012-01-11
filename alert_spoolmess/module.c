5b99392ad961f1bfb873aaa24163a0a92a127b15
Jean-Francois Smigielski <jean-francois.smigielski@atosorigin.com>
2012-01-11 18:29:27 +0100

    Removes a useless dependency to metautils

--- alert_spoolmess/module.c
+++ alert_spoolmess/module.c
@@ -10,9 +10,20 @@
 
 #include <glib.h>
 
-#include <metautils.h>
-
 #include "../main/gridinit_alerts.h"
+
+# define JOURNAL(Prio,Format,...) log4c_category_log(log4c_category_get(LOG_DOMAIN), \
+	Prio, "%u "Format, getpid(), ##__VA_ARGS__)
+
+# define FATAL(Format,...)  JOURNAL(LOG4C_PRIORITY_FATAL,  Format, ##__VA_ARGS__)
+# define ALERT(Format,...)  JOURNAL(LOG4C_PRIORITY_ALERT,  Format, ##__VA_ARGS__)
+# define CRIT(Format,...)   JOURNAL(LOG4C_PRIORITY_CRIT,   Format, ##__VA_ARGS__)
+# define ERROR(Format,...)  JOURNAL(LOG4C_PRIORITY_ERROR,  Format, ##__VA_ARGS__)
+# define WARN(Format,...)   JOURNAL(LOG4C_PRIORITY_WARN,   Format, ##__VA_ARGS__)
+# define NOTICE(Format,...) JOURNAL(LOG4C_PRIORITY_NOTICE, Format, ##__VA_ARGS__)
+# define INFO(Format,...)   JOURNAL(LOG4C_PRIORITY_INFO,   Format, ##__VA_ARGS__)
+# define DEBUG(Format,...)  JOURNAL(LOG4C_PRIORITY_DEBUG,  Format, ##__VA_ARGS__)
+# define TRACE(Format,...)  JOURNAL(LOG4C_PRIORITY_TRACE,  Format, ##__VA_ARGS__)
 
 #define DEFAULT_SOPCODE "MUT-GRD-7000"
 
