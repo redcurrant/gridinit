5af1f577afeddcec88af8ed3aee3262f231aba58
Jean-Francois SMIGIELSKI <jean-francois.smigielski@atos.net>
2014-06-18 14:29:36 +0200

    Updated to v1.2.0

--- lib/children.c
+++ lib/children.c
@@ -1,20 +1,3 @@
-/*
- * Copyright (C) 2013 AtoS Worldline
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- * 
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #ifdef HAVE_CONFIG_H
 # include "../config.h"
 #endif
@@ -363,7 +346,7 @@ _child_exec(struct child_s *sd, int argc, char ** args)
 	supervisor_children_cleanall();
 
 	if (NULL == (real_cmd = g_find_program_in_path(cmd)))
-		FATAL("'%s' not found in PATH:%s", cmd, g_getenv("PATH"));
+		FATAL("'%s' not executable or not found in PATH:%s", cmd, g_getenv("PATH"));
 	else {
 		execve(real_cmd, args, env);
 		FATAL("exec failed : errno=%d %s", errno, strerror(errno));
